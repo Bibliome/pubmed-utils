34feb0cce06ebd11552ef1effb1a66314d8b69c2
Robert Bossy <Robert.Bossy@inra.fr>
2017-09-14 12:23:58 +0200

    added option --no-doctype

--- split-pubmed.py
+++ split-pubmed.py
@@ -10,6 +10,9 @@ XML_HEADER = '''<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE PubmedArticleSet SYSTEM "http://dtd.nlm.nih.gov/ncbi/pubmed/out/pubmed_170101.dtd">
 <PubmedArticleSet>
 '''
+XML_HEADER_NO_DOCTYPE = '''<?xml version="1.0" encoding="utf-8"?>
+<PubmedArticleSet>
+'''
 XML_FOOTER = '''</PubmedArticleSet>
 '''
 
@@ -18,6 +21,7 @@ class SplitPubMed(OptionParser):
         OptionParser.__init__(self, usage='usage: %prog [options] XMLFILES...')
         self.add_option('--max-entries', action='store', type='int', dest='max_entries', default=1)
         self.add_option('--pattern', action='store', type='string', dest='pattern', default='%d.xml')
+        self.add_option('--no-doctype', action='store_true', dest='no_doctype', default=False)
 
     def open_next(self, batch):
         filename = self.options.pattern % (batch,)
@@ -25,7 +29,10 @@ class SplitPubMed(OptionParser):
         if dir != '' and not exists(dir):
             makedirs(dir)
         result = open(filename, 'w')
-        result.write(XML_HEADER)
+        if self.options.no_doctype:
+            result.write(XML_HEADER_NO_DOCTYPE)
+        else:
+            result.write(XML_HEADER)        
         return result
 
     def close(self, fout):
